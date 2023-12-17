19ba561d76a727b2065521d17dec09ee3fdf9244
Khalil Galalem <144709285+khalil-galalem@users.noreply.github.com>
2023-12-17 13:16:03 +0100

    Create DateConstants.java

---
+++ en/dates/java/DateConstants.java
@@ -0,0 +1,65 @@
+public class DateConstants {
+
+  // Months
+  public static final int JANUARY   = 0;
+  public static final int FEBRUARY  = 1;
+  public static final int MARCH     = 2;
+  public static final int APRIL     = 3;
+  public static final int MAY       = 4;
+  public static final int JUNE      = 5;
+  public static final int JULY      = 6;
+  public static final int AUGUST    = 7;
+  public static final int SEPTEMBER = 8;
+  public static final int OCTOBER   = 9;
+  public static final int NOVEMBER  = 10;
+  public static final int DECEMBER  = 11;
+
+  // Months Full Format
+  public static final String[] MONTHS_UPPERCASE_FULL_FORMAT    = {"JANUARY", "FEBRUARY", "MARCH", "APRIL", "MAY", "JUNE", "JULY", "AUGUST", "SEPTEMBER", "OCTOBER", "NOVEMBER", "DECEMBER"};
+  public static final String[] MONTHS_CAPITALIZED_FULL_FORMAT  = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+  public static final String[] MONTHS_LOWERCASE_FULL_FORMAT    = {"january", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december"};
+
+  // Months Short Format
+  public static final String[] MONTHS_UPPERCASE_SHORT_FORMAT   = {"JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"};
+  public static final String[] MONTHS_CAPITALIZED_SHORT_FORMAT = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
+  public static final String[] MONTHS_LOWERCASE_SHORT_FORMAT   = {"jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sep", "oct", "nov", "dec"};
+
+
+
+  // DAYS
+  public static final int SUNDAY    = 0;
+  public static final int MONDAY    = 1;
+  public static final int TUESDAY   = 2;
+  public static final int WEDNESDAY = 3;
+  public static final int THURSDAY  = 4;
+  public static final int FRIDAY    = 5;
+  public static final int SATURDAY  = 6;
+
+  // Days Full Format
+  public static final String[] DAYS_UPPERCASE_FULL_FORMAT      = {"SUNDAY", "MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY", "SATURDAY"};
+  public static final String[] DAYS_CAPITALIZED_FULL_FORMAT    = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+  public static final String[] DAYS_LOWERCASE_FULL_FORMAT      = {"sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"};
+
+  // Days Medium Format 
+  public static final String[] DAYS_UPPERCASE_MEDIUM_FORMAT    = {"SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"};
+  public static final String[] DAYS_CAPITALIZED_MEDIUM_FORMAT  = {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
+  public static final String[] DAYS_LOWERCASE_MEDIUM_FORMAT    = {"sun", "mon", "tue", "wed", "thu", "fri", "sat"};
+
+  // Days Short Format 
+  public static final String[] DAYS_UPPERCASE_SHORT_FORMAT     = {"SU", "MO", "TU", "WE", "TH", "FR", "SA"};
+  public static final String[] DAYS_CAPITALIZED_SHORT_FORMAT   = {"Su", "Mo", "Tu", "We", "Th", "Fr", "Sa"};
+  public static final String[] DAYS_LOWERCASE_SHORT_FORMAT     = {"su", "mo", "tu", "we", "th", "fr", "sa"};
+
+
+  // Seasons
+  public static final int SPRING = 0;
+  public static final int SUMMER = 1;
+  public static final int AUTUMN = 2;
+  public static final int WINTER = 3;
+
+  // Seasons Full Format
+  public static final String[] SEASONS_UPPERCASE_FULL_FORMAT   = {"SPRING", "SUMMER", "AUTUMN", "WINTER"};
+  public static final String[] SEASONS_CAPITALIZED_FULL_FORMAT = {"Spring", "Summer", "Autumn", "Winter"};
+  public static final String[] SEASONS_LOWERCASE_FULL_FORMAT   = {"spring", "summer", "autumn", "winter"};
+  
+}
