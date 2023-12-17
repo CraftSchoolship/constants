7a21e4f1db3ef3c9cec36670bff612566dcd06ce
Khalil Galalem <144709285+khalil-galalem@users.noreply.github.com>
2023-12-17 13:23:13 +0100

    Create date_constants.py

---
+++ en/dates/python/date_constants.py
@@ -0,0 +1,61 @@
+# Months
+JANUARY   = 0
+FEBRUARY  = 1
+MARCH     = 2
+APRIL     = 3
+MAY       = 4
+JUNE      = 5
+JULY      = 6
+AUGUST    = 7
+SEPTEMBER = 8
+OCTOBER   = 9
+NOVEMBER  = 10
+DECEMBER  = 11
+
+# Months Full Format
+MONTHS_UPPERCASE_FULL_FORMAT    = ["JANUARY", "FEBRUARY", "MARCH", "APRIL", "MAY", "JUNE", "JULY", "AUGUST", "SEPTEMBER", "OCTOBER", "NOVEMBER", "DECEMBER"]
+MONTHS_CAPITALIZED_FULL_FORMAT  = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+MONTHS_LOWERCASE_FULL_FORMAT    = ["january", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december"]
+
+# Months Short Format
+MONTHS_UPPERCASE_SHORT_FORMAT   = ["JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"]
+MONTHS_CAPITALIZED_SHORT_FORMAT = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+MONTHS_LOWERCASE_SHORT_FORMAT   = ["jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sep", "oct", "nov", "dec"]
+
+
+
+# DAYS
+SUNDAY    = 0
+MONDAY    = 1
+TUESDAY   = 2
+WEDNESDAY = 3
+THURSDAY  = 4
+FRIDAY    = 5
+SATURDAY  = 6
+
+# Days Full Format
+DAYS_UPPERCASE_FULL_FORMAT      = ["SUNDAY", "MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY", "SATURDAY"]
+DAYS_CAPITALIZED_FULL_FORMAT    = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+DAYS_LOWERCASE_FULL_FORMAT      = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"]
+
+# Days Medium Format 
+DAYS_UPPERCASE_MEDIUM_FORMAT    = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"]
+DAYS_CAPITALIZED_MEDIUM_FORMAT  = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
+DAYS_LOWERCASE_MEDIUM_FORMAT    = ["sun", "mon", "tue", "wed", "thu", "fri", "sat"]
+
+# Days Short Format 
+DAYS_UPPERCASE_SHORT_FORMAT     = ["SU", "MO", "TU", "WE", "TH", "FR", "SA"]
+DAYS_CAPITALIZED_SHORT_FORMAT   = ["Su", "Mo", "Tu", "We", "Th", "Fr", "Sa"]
+DAYS_LOWERCASE_SHORT_FORMAT     = ["su", "mo", "tu", "we", "th", "fr", "sa"]
+
+
+# Seasons
+SPRING = 0
+SUMMER = 1
+AUTUMN = 2
+WINTER = 3
+
+# Seasons Full Format
+SEASONS_UPPERCASE_FULL_FORMAT   = ["SPRING", "SUMMER", "AUTUMN", "WINTER"]
+SEASONS_CAPITALIZED_FULL_FORMAT = ["Spring", "Summer", "Autumn", "Winter"]
+SEASONS_LOWERCASE_FULL_FORMAT   = ["spring", "summer", "autumn", "winter"]
